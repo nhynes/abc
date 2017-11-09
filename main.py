673ab671e56a9ad80235e9ede293283c33f11a79
Nick Hynes <nhynes@berkeley.edu>
2017-11-09 13:12:31 -0800

    Improve logging

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('--env', choices=environ.ENVS, default=environ.SYNTH)
     parser.add_argument('--resume', action='store_true')
     parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--log-prefix')
     init_opts, remaining_opts = parser.parse_known_args()
 
     opts_file = os.path.join(RUN_DIR, 'opts.pkl')
@@ -28,7 +29,7 @@ def main():
             init_opts, remaining_opts, no_defaults=True)
         opts = argparse.Namespace(**common.unpickle(opts_file))
         for k, v in vars(new_opts).items():
-            if v is not None:
+            if k not in opts or v is not None:
                 setattr(opts, k, v)
     else:
         opts = environ.parse_env_opts(init_opts, remaining_opts)
@@ -36,7 +37,7 @@ def main():
         with open(opts_file, 'wb') as f_opts:
             pickle.dump(vars(opts), f_opts)
 
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG, filemode='w')
     logger = logging.getLogger()
 
     torch.manual_seed(opts.seed)
@@ -45,15 +46,16 @@ def main():
     env = environ.create(opts.env, opts)
 
     for phase in PHASES:
-        logger.debug(f'Beginning phase: {phase}')
         torch.manual_seed(opts.seed)
         torch.cuda.manual_seed_all(opts.seed)
-        with _phase(env, phase) as phase_runner:
-            phase_runner()
+        with _phase(env, phase, opts) as phase_runner:
+            if phase_runner:
+                logger.debug(f'# running phase: {phase}')
+                phase_runner()
 
 
 @contextmanager
-def _phase(env, phase):
+def _phase(env, phase, opts):
     phase_dir = os.path.join(RUN_DIR, phase)
     if not os.path.isdir(phase_dir):
         os.mkdir(phase_dir)
@@ -61,7 +63,7 @@ def _phase(env, phase):
     snap_file = os.path.join(phase_dir, 'state.pth')
     if os.path.isfile(snap_file):
         env.state = torch.load(snap_file)
-        yield lambda: None
+        yield None
         return
 
     if phase == G_ML:
@@ -72,9 +74,15 @@ def _phase(env, phase):
         runner = env.train_adv
 
     logger = logging.getLogger()
-    file_logger = logging.FileHandler(os.path.join(phase_dir, 'log.txt'))
-    file_logger.setLevel(logging.INFO)
-    logger.addHandler(file_logger)
+    def _add_file_handler(lvl, prefixes=[]):
+        log_path = os.path.join(phase_dir, '_'.join(prefixes + ['log.txt']))
+        handler = logging.FileHandler(log_path, mode='w')
+        handler.setLevel(lvl)
+        logger.addHandler(handler)
+
+    log_prefixes = [opts.log_prefix]*bool(opts.log_prefix)
+    _add_file_handler(logging.INFO, log_prefixes)
+    _add_file_handler(logging.DEBUG, log_prefixes + ['debug'])
 
     yield runner
 
--- scripts/make_links.sh
+++ scripts/make_links.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 ln -Lrsf ../../environ/environment.py environ
 ln -Lrsf ../../environ/synth.py environ
 ln -Lrsf ../../model/discriminator.py model
 ln -Lrsf ../../model/generator.py model
 ln -Lrsf ../../model/utils.py model
+ln -Lrsf ../../main.py .
