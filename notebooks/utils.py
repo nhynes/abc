008e81e2888279d84e4054f7bd26160ca6ad2b90
Nick Hynes <nhynes@berkeley.edu>
2017-11-12 13:59:55 -0800

    Add log loader to notebook utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,27 @@
 """Utilities for analyzing results in notebooks."""
 
+from collections import defaultdict
+import re
+
 import torch
 import numpy as np
+import pandas as pd
+
+
+FG = r'(\d+\.\d+)'  # Float Group
+LOG_RES = {
+    'iter': r'\[(\d+)]',
+    'nll': rf'nll: {FG}',
+    'acc': {
+        'expr': rf'acc: o={FG} g={FG}',
+        'groups': ['o', 'g'],
+    },
+    'gnorm': {
+        'expr': rf'gnorm: g={FG} d={FG}',
+        'groups': ['g', 'd'],
+    },
+}
+
 
 def rolling_window_lastaxis(a, window):
     """Directly taken from Erik Rigtorp's post to numpy-discussion.
@@ -14,13 +34,17 @@ def rolling_window_lastaxis(a, window):
     strides = a.strides + (a.strides[-1],)
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
+
 def ngram_count(seqs, n):
+    """Returns a dict of {ngram: counts} for a numpy array of tokens."""
     rw = rolling_window_lastaxis(seqs, n).reshape(-1, n)
     ngrams, counts = np.unique(rw, axis=0, return_counts=True)
     counts = counts / counts.sum()
     return {tuple(ngram): count for ngram, count in zip(ngrams, counts)}
 
+
 def sample_gen(env, num_samps=10000, gen='g', return_probs=False):
+    """Samples from a generative model."""
     samps = []
     probs = []
     gen = getattr(env, gen)
@@ -35,3 +59,42 @@ def sample_gen(env, num_samps=10000, gen='g', return_probs=False):
     if return_probs:
         return samps, torch.cat(probs, 0).numpy()
     return samps
+
+
+def load_log(log_path):
+    """Loads a log file."""
+
+    if not LOG_RES.get('_compiled'):
+        for stat, spec in LOG_RES.items():
+            if isinstance(spec, dict):
+                spec['expr'] = re.compile(spec['expr'])
+            else:
+                LOG_RES[stat] = re.compile(spec)
+        LOG_RES['_compiled'] = True
+
+    cols = defaultdict(list)
+    with open(log_path) as f_log:
+        for line in f_log:
+            if line.startswith('#'):
+                continue
+            for stat, spec in sorted(LOG_RES.items()):
+                if stat.startswith('_'):
+                    continue
+
+                if isinstance(spec, dict):
+                    matcher = spec['expr']
+                    colnames = [f'{stat}_{substat}'
+                                for substat in spec['groups']]
+                else:
+                    matcher = spec
+                    colnames = [stat]
+
+                match = matcher.search(line)
+                if not match:
+                    continue
+
+                for colname, val in zip(colnames, match.groups()):
+                    cols[colname].append(val)
+    log_df = pd.DataFrame.from_dict(cols)
+    log_df = log_df.set_index('iter').astype(float)
+    return log_df
